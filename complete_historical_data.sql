b17388b3ef9432732e37cc0867ab4e5ed94e74f4
Christopher-Shevlin <48866944+ChrisJamesShevlin@users.noreply.github.com>
2024-02-19 20:12:09 +0000

    Add files via upload

---
+++ complete_historical_data.sql
@@ -0,0 +1,48 @@
+
+CREATE DATABASE IF NOT EXISTS historical_data;
+USE historical_data;
+
+
+CREATE TABLE data
+(
+    Timestamp VARCHAR(512),
+    Open DOUBLE,
+    High DOUBLE,
+    Low DOUBLE,
+    Close DOUBLE
+);
+
+INSERT INTO data (Timestamp, Open, High, Low, Close) VALUES ('2023:10:22-00:00:00', '4232.41', '4223.79', '4235.79', '4234.29');
+INSERT INTO data (Timestamp, Open, High, Low, Close) VALUES ('2023:10:23-00:00:00', '4234.04', '4189.28', '4255.56', '4227.28');
+INSERT INTO data (Timestamp, Open, High, Low, Close) VALUES ('2023:10:24-00:00:00', '4227.03', '4218.48', '4266.43', '4244.2');
+INSERT INTO data (Timestamp, Open, High, Low, Close) VALUES ('2023:10:25-00:00:00', '4243.83', '4168.33', '4246.08', '4172.58');
+INSERT INTO data (Timestamp, Open, High, Low, Close) VALUES ('2023:10:26-00:00:00', '4172.33', '4126.96', '4184.28', '4154.43');
+INSERT INTO data (Timestamp, Open, High, Low, Close) VALUES ('2023:10:27-00:00:00', '4154.68', '4103.19', '4164.68', '4116.15');
+INSERT INTO data (Timestamp, Open, High, Low, Close) VALUES ('2023:10:29-00:00:00', '4130.33', '4124.46', '4133.96', '4130.21');
+INSERT INTO data (Timestamp, Open, High, Low, Close) VALUES ('2023:10:30-00:00:00', '4129.96', '4129.21', '4177.38', '4162.14');
+INSERT INTO data (Timestamp, Open, High, Low, Close) VALUES ('2023:10:31-00:00:00', '4162.39', '4147.38', '4195.45', '4183.08');
+INSERT INTO data (Timestamp, Open, High, Low, Close) VALUES ('2023:11:01-00:00:00', '4182.95', '4172.48', '4248.13', '4247.63');
+INSERT INTO data (Timestamp, Open, High, Low, Close) VALUES ('2023:11:02-00:00:00', '4247.38', '4244.08', '4320.75', '4310.4');
+INSERT INTO data (Timestamp, Open, High, Low, Close) VALUES ('2023:11:03-00:00:00', '4310.65', '4308.77', '4373.38', '4360.15');
+INSERT INTO data (Timestamp, Open, High, Low, Close) VALUES ('2023:11:05-00:00:00', '4364.48', '4357.61', '4365.86', '4359.36');
+INSERT INTO data (Timestamp, Open, High, Low, Close) VALUES ('2023:11:06-00:00:00', '4359.11', '4346.98', '4371.88', '4359.23');
+INSERT INTO data (Timestamp, Open, High, Low, Close) VALUES ('2023:11:07-00:00:00', '4359.48', '4349.08', '4386.07', '4378.38');
+INSERT INTO data (Timestamp, Open, High, Low, Close) VALUES ('2023:11:08-00:00:00', '4378.25', '4358.89', '4390.82', '4376.23');
+INSERT INTO data (Timestamp, Open, High, Low, Close) VALUES ('2023:11:09-00:00:00', '4376.1', '4341.59', '4397.58', '4344.34');
+INSERT INTO data (Timestamp, Open, High, Low, Close) VALUES ('2023:11:10-00:00:00', '4344.09', '4338.59', '4419.27', '4407.96');
+INSERT INTO data (Timestamp, Open, High, Low, Close) VALUES ('2023:11:12-00:00:00', '4411.31', '4404.69', '4413.44', '4405.94');
+INSERT INTO data (Timestamp, Open, High, Low, Close) VALUES ('2023:11:13-00:00:00', '4405.81', '4392.84', '4422.52', '4412.02');
+INSERT INTO data (Timestamp, Open, High, Low, Close) VALUES ('2023:11:14-00:00:00', '4411.77', '4409.77', '4509.08', '4500.39');
+INSERT INTO data (Timestamp, Open, High, Low, Close) VALUES ('2023:11:15-00:00:00', '4500.51', '4495.35', '4524.4', '4500.11');
+INSERT INTO data (Timestamp, Open, High, Low, Close) VALUES ('2023:11:16-00:00:00', '4499.86', '4487.26', '4514.43', '4509.69');
+INSERT INTO data (Timestamp, Open, High, Low, Close) VALUES ('2023:11:17-00:00:00', '4509.94', '4499.24', '4525.53', '4508.85');
+INSERT INTO data (Timestamp, Open, High, Low, Close) VALUES ('2023:11:19-00:00:00', '4513.59', '4506.97', '4515.22', '4510.72');
+INSERT INTO data (Timestamp, Open, High, Low, Close) VALUES ('2023:11:20-00:00:00', '4510.97', '4506.47', '4557.45', '4550.49');
+INSERT INTO data (Timestamp, Open, High, Low, Close) VALUES ('2023:11:21-00:00:00', '4550.24', '4524.12', '4553.78', '4533.3');
+INSERT INTO data (Timestamp, Open, High, Low, Close) VALUES ('2023:11:22-00:00:00', '4533.05', '4533.05', '4569.72', '4552.62');
+INSERT INTO data (Timestamp, Open, High, Low, Close) VALUES ('2023:11:23-00:00:00', '4552.37', '4551.37', '4561.97', '4560.12');
+INSERT INTO data (Timestamp, Open, High, Low, Close) VALUES ('2023:11:24-00:00:00', '4559.87', '4553.07', '4563.8', '4554.97');
+INSERT INTO data (Timestamp, Open, High, Low, Close) VALUES ('2023:11:26-00:00:00', '4556.51', '4551.64', '4556.64', '4552.39');
+INSERT INTO data (Timestamp, Open, High, Low, Close) VALUES ('2023:11:27-00:00:00', '4552.14', '4542.64', '4560.4', '4554.68');
+-- Additional INSERT statements truncated for brevity
+
